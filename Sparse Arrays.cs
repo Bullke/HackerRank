33dce9d0f98ca5d3d545419dfb7081afc13ceeec
Bullke <nbullke@gmail.com>
2017-03-31 12:29:50 -0700

    Add files via upload

---
+++ Sparse Arrays.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+	public class Node
+	{
+
+		private int count = 0;
+		private int depth = 0;
+		private Node[] subnode = null;
+
+		private Node[] Subnode
+		{
+			get
+			{
+				if (subnode == null)
+				{
+					subnode = new Node[52];
+				}
+				return subnode;
+			}
+		}
+
+		public Node() : this(0) {}
+
+		private Node(int input)
+		{
+			this.depth = input;
+		}
+
+		public int Add(string input)
+		{
+			Node current = this;
+			int index = 0;
+			for (int position = 0; position < input.Length; position++)
+			{
+				index = CharIndex(input[position]);
+				if (current.Subnode[index] == null)
+				{
+					current.Subnode[index] = new Node(depth + 1);
+				}
+				current = current.Subnode[index];
+			}
+			current.count++;
+			return current.count;
+		}
+
+		private int CharIndex(char input)
+		{
+			int index = 0;
+			if ((input >= 'A') && (input <= 'Z'))
+			{
+				index = (input - 'A');
+			}
+			else if ((input >= 'a') && (input <= 'z'))
+			{
+				index = (input - 'a');
+			}
+			else
+			{
+				throw new ArgumentException("Input must be an alphabetical character");
+			}
+			return index;
+		}
+
+		public int CountOf(string input)
+		{
+			Node current = this;
+			int index;
+			for (int position = 0; position < input.Length; position++)
+			{
+				index = CharIndex(input[position]);
+				if (current.Subnode[index] == null)
+				{
+					return 0;
+				}
+				else
+				{
+					current = current.Subnode[index];
+				}
+			}
+			return current.count;
+		}
+	}
+
+	static void Main(String[] args)
+	{
+		
+	}
+}
---
+++ StringSearch.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+	public class Node
+	{
+
+		private int count = 0;
+		private int depth = 0;
+		private Node[] subnode = null;
+
+		public Node()
+		{
+			subnode = new Node[52];
+		}
+
+		public int Add(string input)
+		{
+			Node current = this;
+			int index = 0;
+			for (int position = 0; position < input.Length; position++)
+			{
+				index = CharIndex(input[position]);
+				if (current.Subnode[index] == null)
+				{
+					current.Subnode[index] = new Node(depth + 1);
+				}
+				current = current.Subnode[index];
+			}
+			current.count++;
+			return current.count;
+		}
+
+		private int CharIndex(char input)
+		{
+			int index = 0;
+			if ((input >= 'A') && (input <= 'Z'))
+			{
+				index = (input - 'A');
+			}
+			else if ((input >= 'a') && (input <= 'z'))
+			{
+				index = (input - 'a');
+			}
+			else
+			{
+				throw new ArgumentException("Input must be an alphabetical character");
+			}
+			return index;
+		}
+
+		public int CountOf(string input)
+		{
+			Node current = this;
+			int index;
+			for (int position = 0; position < input.Length; position++)
+			{
+				index = CharIndex(input[position]);
+				if (current.Subnode[index] == null)
+				{
+					return 0;
+				}
+				else
+				{
+					current = current.Subnode[index];
+				}
+			}
+			return current.count;
+		}
+	}
+
+	static void Main(String[] args)
+	{
+		
+	}
+}
